dc56a091ab7b7676f1bfdc59199c6b762d6d0c0a
Akarachai Phukitisakul <akarachai.phu@TDG-M0767.local>
2025-11-04 13:53:55 +0700

    test: auto deploy flow with approval

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Preprod Mock API! 🚀");
+  res.send("Hello from Preprod Mock API V3! 🚀");
 });
 
 app.listen(8080, () => console.log("Server running on port 8080"));
