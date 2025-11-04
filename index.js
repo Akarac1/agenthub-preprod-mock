b5806d91ef82ad3be9b4d714aacc92c8f713aa30
Akarachai Phukitisakul <akarachai.phu@TDG-M0767.local>
2025-11-04 14:03:51 +0700

    chore: trigger cloud build test

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get("/", (req, res) => {
   res.send("Hello from Preprod Mock API V3! 🚀");
 });
 
-app.listen(8080, () => console.log("Server running on port 8080"));
+app.listen(8080, () => console.log("Server running on port 8080"));// trigger test Tue Nov  4 14:03:51 +07 2025
