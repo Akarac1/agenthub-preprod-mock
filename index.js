066a30f0d23927358d0abf63ce064ff901e513f3
Akarachai Phukitisakul <akarachai.phu@TDG-M0767.local>
2025-11-05 10:26:23 +0700

    Update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Preprod Mock API V3! 🚀");
+  res.send("Hello from Preprod Mock API V4! 🚀");
 });
 
 app.listen(8080, () => console.log("Server running on port 8080"));// trigger test Tue Nov  4 14:03:51 +07 2025
